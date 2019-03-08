ee98dd9b8f79539443c3fee3b37da7223d70bde3
Gleb Siroki <g.shiroki@gmail.com>
2019-03-08 13:56:16 +0000

    Cleaned up according to PEP8

--- test_tmm.py
+++ test_tmm.py
@@ -1,10 +1,9 @@
-import pytest
 import os
 import subprocess as sp
 
+import pytest
 import toml
-    
-
+# TODO add docstring
 def make_layer_dic(epsilon, mu, thickness):
     return {'epsilon': epsilon, 'mu': mu, 'thickness': thickness}
 
@@ -13,6 +12,7 @@ def make_source_dic(wavelength, theta, phi, te_amplitude, tm_amplitude):
             'te_amplitude': te_amplitude, 'tm_amplitude': tm_amplitude}
 
 def test_benchmark():
+    sp.call('rm output.toml', shell=True)
     source_dic = make_source_dic(1, 57, 23, [1, 0], [0, 1])
     superstrate_dic = {'mu': 1.2, 'epsilon': 1.4}
     layer_1_dic = make_layer_dic(2, 1, 0.25)
@@ -24,10 +24,10 @@ def test_benchmark():
     input_toml_path = os.path.join(os.getcwd(), 'input.toml')
     with open(input_toml_path, 'w') as fid:
         toml.dump(input_toml, fid)
-    
+
     # TODO abs path here and for output
     sp.call('python tmm.py ' + input_toml_path, shell=True)
     output_toml = toml.load('output.toml')
-    assert output_toml['output']['R'] == 0.4403
-    assert output_toml['output']['T'] == 0.5597
-    assert output_toml['output']['R+T'] == 1
+    assert output_toml['output']['ref_eff'] == 0.4403
+    assert output_toml['output']['trn_eff'] == 0.5597
+    assert output_toml['output']['ref_eff+trn_eff'] == 1
--- tmm.py
+++ tmm.py
@@ -2,29 +2,31 @@
 
 # transfer matrix method implemented as python
 # based on the notes byhttp://emlab.utep.edu/ee5390cem.htm
-import numpy as np
 import argparse
 import os
-import toml
 import cmath
 
+import toml
+import numpy as np
+#TODO add docstrings
 def calc_gap_layer_params(kx, ky):
     ur = 1
     er = 1
-    Q = np.array(([kx*ky, ur*er+ky*ky],[-(ur*er+kx*kx), -kx*ky]))/ur
-    V = -1j*Q
-    return V
+    q_mat = np.array(([kx*ky, ur*er+ky*ky], [-(ur*er+kx*kx), -kx*ky]))/ur
+    v_mat = -1j*q_mat
+    return v_mat
 
 def calc_layer_params(ur, er, kx, ky):
-    Q = np.array(([kx*ky, ur*er-kx*kx],[ky*ky-ur*er, -kx*ky]))/ur
+    q_mat = np.array(([kx*ky, ur*er-kx*kx], [ky*ky-ur*er, -kx*ky]))/ur
     kz = cmath.sqrt(ur*er-kx*kx-ky*ky)
-    Omega = 1j*kz*np.array(([1, 0], [0, 1]))
-    V = np.matmul(Q, np.linalg.inv(Omega))
-    return Omega, V
-    
-def init_global_S_mat():
-    S_global = np.array(([0, 0, 1, 0], [0, 0, 0, 1], [1, 0, 0, 0], [0, 1, 0, 0]))
-    return S_global
+    omega_mat = 1j*kz*np.array(([1, 0], [0, 1]))
+    v_mat = np.matmul(q_mat, np.linalg.inv(omega_mat))
+    return omega_mat, v_mat
+
+def init_global_s_mat():
+    s_global = np.array(([0, 0, 1, 0], [0, 0, 0, 1],
+                         [1, 0, 0, 0], [0, 1, 0, 0]))
+    return s_global
 
 def matmul(*args):
     if len(args) == 1 or len(args) == 0:
@@ -39,70 +41,76 @@ def matmul(*args):
         count += 1
     return ret
 
-def calc_S_mat(L, ur, er, kx, ky, I, K0):
-    Omegai, Vi = calc_layer_params(ur, er, kx, ky)
-    Vg = calc_gap_layer_params(kx, ky)
-    Ai = I + np.matmul(np.linalg.inv(Vi), Vg)
-    Bi = I - np.matmul(np.linalg.inv(Vi), Vg)
-    Xi = np.diag(np.exp(np.diag(Omegai)*K0*L))
-    Ai_inv = np.linalg.inv(Ai)
-    Di = Ai - matmul(Xi, Bi, Ai_inv, Xi, Bi)
-    Di_inv = np.linalg.inv(Di)
-    S_11 = matmul(Di_inv, matmul(Xi, Bi, Ai_inv, Xi, Ai) - Bi)
-    S_12 = matmul(Di_inv, Xi, Ai - matmul(Bi, Ai_inv, Bi))
+def calc_s_mat(layer_thickness, ur, er, kx, ky, unit_mat, k0):
+    omegai_mat, vi_mat = calc_layer_params(ur, er, kx, ky)
+    vg_mat = calc_gap_layer_params(kx, ky)
+    ai_mat = unit_mat + np.matmul(np.linalg.inv(vi_mat), vg_mat)
+    bi_mat = unit_mat - np.matmul(np.linalg.inv(vi_mat), vg_mat)
+    xi_mat = np.diag(np.exp(np.diag(omegai_mat)*k0*layer_thickness))
+    ai_inv_mat = np.linalg.inv(ai_mat)
+    di_mat = ai_mat - matmul(xi_mat, bi_mat, ai_inv_mat, xi_mat, bi_mat)
+    di_inv_mat = np.linalg.inv(di_mat)
+    s_11_mat = matmul(di_inv_mat, matmul(
+        xi_mat, bi_mat, ai_inv_mat, xi_mat, ai_mat) - bi_mat)
+    s_12_mat = matmul(di_inv_mat, xi_mat, ai_mat
+                      - matmul(bi_mat, ai_inv_mat, bi_mat))
     # S_12 = S_21, S_11 = S_22
-    S = np.concatenate((np.concatenate((S_11, S_12), axis = 1),
-                               np.concatenate((S_12, S_11), axis = 1)))
-    return S
+    s_mat = np.concatenate((np.concatenate((s_11_mat, s_12_mat), axis=1),
+                            np.concatenate((s_12_mat, s_11_mat), axis=1)))
+    return s_mat
 
-def redheffer_star_prod(Sa_4x4, Sb_4x4, I):
-    Sa_11 = Sa_4x4[np.ix_([0, 1], [0, 1])]
-    Sa_12 = Sa_4x4[np.ix_([0, 1], [2, 3])]
-    Sa_21 = Sa_4x4[np.ix_([2, 3], [0, 1])]
-    Sa_22 = Sa_4x4[np.ix_([2, 3], [2, 3])]
-    
-    Sb_11 = Sb_4x4[np.ix_([0, 1], [0, 1])]
-    Sb_12 = Sb_4x4[np.ix_([0, 1], [2, 3])]
-    Sb_21 = Sb_4x4[np.ix_([2, 3], [0, 1])]
-    Sb_22 = Sb_4x4[np.ix_([2, 3], [2, 3])]
-    
-    D = matmul(Sa_12, np.linalg.inv(I - matmul(Sb_11, Sa_22)))
-    F = matmul(Sb_21, np.linalg.inv(I - matmul(Sa_22, Sb_11)))
-    
-    S_11 = Sa_11 + matmul(D, Sb_11, Sa_21)
-    S_12 = matmul(D, Sb_12)
-    S_21 = matmul(F, Sa_21)
-    S_22 = Sb_22 + matmul(F, Sa_22, Sb_12)
-    
-    S_ret = np.concatenate((np.concatenate((S_11, S_12), axis = 1),
-                               np.concatenate((S_21, S_22), axis = 1)))
-    return S_ret
+def redheffer_star_prod(sa_4x4_mat, sb_4x4_mat, unit_mat):
+    sa_11_mat = sa_4x4_mat[np.ix_([0, 1], [0, 1])]
+    sa_12_mat = sa_4x4_mat[np.ix_([0, 1], [2, 3])]
+    sa_21_mat = sa_4x4_mat[np.ix_([2, 3], [0, 1])]
+    sa_22_mat = sa_4x4_mat[np.ix_([2, 3], [2, 3])]
 
-def calc_S_ref(ur, er, kx, ky, I):
-    Omega_ref, V_ref = calc_layer_params(ur, er, kx, ky)
-    Vg = calc_gap_layer_params(kx, ky)
-    A_ref = I + matmul(np.linalg.inv(Vg), V_ref)
-    B_ref = I - matmul(np.linalg.inv(Vg), V_ref)
-    S_ref_11 = -matmul(np.linalg.inv(A_ref), B_ref)
-    S_ref_12 = 2*np.linalg.inv(A_ref)
-    S_ref_21 = 0.5*(A_ref - matmul(B_ref, np.linalg.inv(A_ref), B_ref))
-    S_ref_22 = matmul(B_ref, np.linalg.inv(A_ref))
-    S_ref = np.concatenate((np.concatenate((S_ref_11, S_ref_12), axis = 1),
-                                np.concatenate((S_ref_21, S_ref_22), axis = 1)))
-    return S_ref
+    sb_11_mat = sb_4x4_mat[np.ix_([0, 1], [0, 1])]
+    sb_12_mat = sb_4x4_mat[np.ix_([0, 1], [2, 3])]
+    sb_21_mat = sb_4x4_mat[np.ix_([2, 3], [0, 1])]
+    sb_22_mat = sb_4x4_mat[np.ix_([2, 3], [2, 3])]
 
-def calc_S_trn(ur, er, kx, ky, I):
-    Omega_trn, V_trn = calc_layer_params(ur, er, kx, ky)
-    Vg = calc_gap_layer_params(kx, ky)
-    A_trn = I + matmul(np.linalg.inv(Vg), V_trn)
-    B_trn = I - matmul(np.linalg.inv(Vg), V_trn)
-    S_trn_11 =  matmul(B_trn, np.linalg.inv(A_trn))
-    S_trn_12 = 0.5*(A_trn - matmul(B_trn, np.linalg.inv(A_trn), B_trn))
-    S_trn_21 = 2*np.linalg.inv(A_trn)
-    S_trn_22 = -matmul(np.linalg.inv(A_trn), B_trn)
-    S_trn = np.concatenate((np.concatenate((S_trn_11, S_trn_12), axis = 1),
-                                   np.concatenate((S_trn_21, S_trn_22), axis = 1)))
-    return S_trn
+    d_mat = matmul(sa_12_mat, np.linalg.inv(unit_mat - matmul(sb_11_mat, sa_22_mat)))
+    f_mat = matmul(sb_21_mat, np.linalg.inv(unit_mat - matmul(sa_22_mat, sb_11_mat)))
+
+    s_11_mat = sa_11_mat + matmul(d_mat, sb_11_mat, sa_21_mat)
+    s_12_mat = matmul(d_mat, sb_12_mat)
+    s_21_mat = matmul(f_mat, sa_21_mat)
+    s_22_mat = sb_22_mat + matmul(f_mat, sa_22_mat, sb_12_mat)
+
+    s_ret_mat = np.concatenate((np.concatenate((s_11_mat, s_12_mat), axis=1),
+                                np.concatenate((s_21_mat, s_22_mat), axis=1)))
+    return s_ret_mat
+
+def calc_s_ref(ur, er, kx, ky, unit_mat):
+    omega_ref_mat, v_ref_mat = calc_layer_params(ur, er, kx, ky)
+    vg_mat = calc_gap_layer_params(kx, ky)
+    a_ref_mat = unit_mat + matmul(np.linalg.inv(vg_mat), v_ref_mat)
+    b_ref_mat = unit_mat - matmul(np.linalg.inv(vg_mat), v_ref_mat)
+    s_ref_11_mat = -matmul(np.linalg.inv(a_ref_mat), b_ref_mat)
+    s_ref_12_mat = 2*np.linalg.inv(a_ref_mat)
+    s_ref_21_mat = 0.5*(a_ref_mat
+                        - matmul(b_ref_mat, np.linalg.inv(a_ref_mat), b_ref_mat))
+    s_ref_22_mat = matmul(b_ref_mat, np.linalg.inv(a_ref_mat))
+    s_ref_mat = np.concatenate((
+        np.concatenate((s_ref_11_mat, s_ref_12_mat), axis=1),
+        np.concatenate((s_ref_21_mat, s_ref_22_mat), axis=1)))
+    return s_ref_mat
+
+def calc_s_trn(ur, er, kx, ky, unit_mat):
+    omega_trn_mat, v_trn_mat = calc_layer_params(ur, er, kx, ky)
+    vg_mat = calc_gap_layer_params(kx, ky)
+    a_trn_mat = unit_mat + matmul(np.linalg.inv(vg_mat), v_trn_mat)
+    b_trn_mat = unit_mat - matmul(np.linalg.inv(vg_mat), v_trn_mat)
+    s_trn_11_mat = matmul(b_trn_mat, np.linalg.inv(a_trn_mat))
+    s_trn_12_mat = 0.5*(a_trn_mat - matmul(b_trn_mat,
+                                           np.linalg.inv(a_trn_mat), b_trn_mat))
+    s_trn_21_mat = 2*np.linalg.inv(a_trn_mat)
+    s_trn_22_mat = -matmul(np.linalg.inv(a_trn_mat), b_trn_mat)
+    s_trn_mat = np.concatenate((
+        np.concatenate((s_trn_11_mat, s_trn_12_mat), axis=1),
+        np.concatenate((s_trn_21_mat, s_trn_22_mat), axis=1)))
+    return s_trn_mat
 
 
 def main():
@@ -114,89 +122,92 @@ def main():
 
     input_toml = toml.load(args.path)
     #print(input_toml)
-    # UNITS
-    DEGREES = np.pi/180
+    degrees_to_rad = np.pi/180
 
-    # SOURCE PARAMETERS # TODO test for deviations from lambda = 2*pi
+    # source parameters
     norm_lambda = 2*np.pi/input_toml['source']['wavelength']
     # free space wavevector; layer thickness normalised below
-    K0 = 1
-    I = np.array(([1, 0], [0, 1])) # 2x2 identity matrix used in many places
-    THETA = input_toml['source']['theta'] * DEGREES #elevation angle
-    PHI = input_toml['source']['phi'] * DEGREES #azimuthal angle
-    P_TE = input_toml['source']['te_amplitude'][0] + \
-            input_toml['source']['te_amplitude'][1]*1j #amplitude of TE polarization
-    P_TM = input_toml['source']['tm_amplitude'][0] + \
-            input_toml['source']['tm_amplitude'][1]*1j #amplitude of TM polarization
+    k0 = 1
+    unit_mat = np.array(([1, 0], [0, 1]))  # 2x2 identity matrix used in many places
+    theta = input_toml['source']['theta'] * degrees_to_rad  # elevation angle
+    phi = input_toml['source']['phi'] * degrees_to_rad  # azimuthal angle
+    p_te = input_toml['source']['te_amplitude'][0] + \
+            input_toml['source']['te_amplitude'][1]*1j  # amplitude of TE polarization
+    p_tm = input_toml['source']['tm_amplitude'][0] + \
+            input_toml['source']['tm_amplitude'][1]*1j  # amplitude of TM polarization
     # normalise polarisation
-    norm_pol = np.sqrt((np.real(P_TE))**2 + (np.imag(P_TE))**2 + \
-    (np.real(P_TM))**2 + (np.imag(P_TM))**2)
-    P_TM = P_TM/norm_pol
-    P_TE = P_TE/norm_pol
+    norm_pol = np.sqrt((np.real(p_te))**2 + (np.imag(p_te))**2 + \
+    (np.real(p_tm))**2 + (np.imag(p_tm))**2)
+    p_tm = p_tm/norm_pol
+    p_te = p_te/norm_pol
 
-    # EXTERNAL MATERIALS
-    #permeability in the reflection region
-    UR1 = input_toml['superstrate']['mu']
-    #permittivity in the reflection region
-    ER1 = input_toml['superstrate']['epsilon']
-    #permeability in the transmission region
-    UR2 = input_toml['substrate']['mu']
-    #permittivity in the transmission region
-    ER2 = input_toml['substrate']['epsilon']
+    # permeability in the reflection region
+    ur1 = input_toml['superstrate']['mu']
+    # permittivity in the reflection region
+    er1 = input_toml['superstrate']['epsilon']
+    # permeability in the transmission region
+    ur2 = input_toml['substrate']['mu']
+    # permittivity in the transmission region
+    er2 = input_toml['substrate']['epsilon']
 
-    # DEFINE LAYERS
+    # define layers
     num_layers = len(input_toml['layer'])
-    UR = [None]*num_layers
-    ER = [None]*num_layers
-    L = [None]*num_layers
+    ur_vec = [None]*num_layers
+    er_vec = [None]*num_layers
+    layer_thicknesses_vec = [None]*num_layers
     for i in range(0, num_layers):
-        UR[i] = input_toml['layer'][i]['mu']
-        ER[i] = input_toml['layer'][i]['epsilon']
-        L[i] = input_toml['layer'][i]['thickness']*norm_lambda
+        ur_vec[i] = input_toml['layer'][i]['mu']
+        er_vec[i] = input_toml['layer'][i]['epsilon']
+        layer_thicknesses_vec[i] = input_toml['layer'][i]['thickness']*norm_lambda
 
-    nr1 = np.sqrt(UR1*ER1)
-    k_inc = K0*nr1*np.array(([np.sin(THETA)*np.cos(PHI), np.sin(THETA)*np.sin(PHI), np.cos(THETA)]))
+    nr1 = np.sqrt(ur1*er1)
+    k_inc = k0*nr1*np.array(([np.sin(theta)*np.cos(phi),
+                              np.sin(theta)*np.sin(phi), np.cos(theta)]))
     kx, ky = k_inc[0], k_inc[1]
     z_unit_vec = np.array(([0, 0, 1]))
-    alpha_TE = np.cross(z_unit_vec, k_inc)
-    alpha_TE = alpha_TE/np.linalg.norm(alpha_TE)
-    alpha_TM = np.cross(alpha_TE, k_inc)
-    alpha_TM = alpha_TM/np.linalg.norm(alpha_TM)
-    E_inc = P_TM*alpha_TM + P_TE*alpha_TE
+    alpha_te = np.cross(z_unit_vec, k_inc)
+    alpha_te = alpha_te/np.linalg.norm(alpha_te)
+    alpha_tm = np.cross(alpha_te, k_inc)
+    alpha_tm = alpha_tm/np.linalg.norm(alpha_tm)
+    E_inc = p_tm*alpha_tm + p_te*alpha_te
     c_inc = np.array(([E_inc[0], E_inc[1], 0, 0]))
 
-    S_global = init_global_S_mat()
-    # calc device S
-    for i in range(0, len(UR)):
-        ur = UR[i]
-        er = ER[i]
-        l = L[i]
-        S_layer = calc_S_mat(l, ur, er, kx, ky, I, K0)
-        S_global = redheffer_star_prod(S_global, S_layer, I)
+    s_global_mat = init_global_s_mat()
+    # take layers into account
+    for i in range(0, num_layers):
+        ur = ur_vec[i]
+        er = er_vec[i]
+        l = layer_thicknesses_vec[i]
+        s_layer_mat = calc_s_mat(l, ur, er, kx, ky, unit_mat, k0)
+        s_global_mat = redheffer_star_prod(s_global_mat, s_layer_mat, unit_mat)
 
-    # calc refl S
-    S_global = redheffer_star_prod(calc_S_ref(UR1, ER1, kx, ky, I), S_global, I)
+    # take superstrate into account
+    s_global_mat = redheffer_star_prod(calc_s_ref(ur1, er1, kx, ky, unit_mat),
+                                       s_global_mat, unit_mat)
 
-    # calc trn S
-    S_global = redheffer_star_prod(S_global, calc_S_trn(UR2, ER2, kx, ky, I), I)
+    # take substrate into account
+    s_global_mat = redheffer_star_prod(s_global_mat,
+                                       calc_s_trn(ur2, er2, kx, ky, unit_mat), unit_mat)
 
-    c_ret = matmul(S_global, c_inc)
-    E_ref = np.array(([c_ret[0], c_ret[1], 0]))
-    E_ref[2] = -(k_inc[0]*E_ref[0]+k_inc[1]*E_ref[1])/k_inc[2]
+    c_ret = matmul(s_global_mat, c_inc)
+    e_field_ref = np.array(([c_ret[0], c_ret[1], 0]))
+    e_field_ref[2] = -(k_inc[0]*e_field_ref[0]+k_inc[1]*e_field_ref[1])/k_inc[2]
     k_trn = np.array(([k_inc[0], k_inc[1], 0]))
-    nr2 = np.sqrt(ER2*UR2)
-    k_trn[2] = np.sqrt(K0*K0*nr2*nr2 - k_trn[0]*k_trn[0] - k_trn[1]*k_trn[1])
-    E_trn = np.array(([c_ret[2], c_ret[3], 0]))
-    E_trn[2] = -(k_trn[0]*E_trn[0]+k_trn[1]*E_trn[1])/(k_trn[2])
-    R = round((np.linalg.norm(E_ref))**2, 4)
-    T = round((np.linalg.norm(E_trn))**2*((k_trn[2]/UR2).real)/((k_inc[2]/UR1).real), 4)
+    nr2 = np.sqrt(er2*ur2)
+    k_trn[2] = np.sqrt(k0*k0*nr2*nr2 - k_trn[0]*k_trn[0] - k_trn[1]*k_trn[1])
+    e_field_trn = np.array(([c_ret[2], c_ret[3], 0]))
+    e_field_trn[2] = -(k_trn[0]*e_field_trn[0]+k_trn[1]*e_field_trn[1])\
+        /(k_trn[2])
+    ref_eff = round((np.linalg.norm(e_field_ref))**2, 4)
+    trn_eff = round((np.linalg.norm(e_field_trn))**2*((k_trn[2]/ur2).real)\
+            /((k_inc[2]/ur1).real), 4)
 
-    print({'R': R, 'T': T, 'R+T': R+T})
+    print({'ref_eff': ref_eff, 'trn_eff': trn_eff,
+           'ref_eff+trn_eff': ref_eff+trn_eff})
     with open('output.toml', 'w') as fid:
-        output = {'output': {r'R': R, r'T': T, r'R+T': R+T}}
+        output = {'output': {r'ref_eff': ref_eff, 'trn_eff': trn_eff,
+                             'ref_eff+trn_eff': ref_eff+trn_eff}}
         toml.dump(output, fid)
 
 if __name__ == '__main__':
     main()
-
-
